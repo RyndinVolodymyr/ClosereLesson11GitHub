fa43f344d8034b954435928198397f6aed9ec0da
Владимир Рындин <ryndinvi@gmail.com>
2018-11-25 21:51:02 +0200

    Initial Commit

---
+++ ClosereLesson11GitHub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClosereLesson11GitHub
+//
+//  Created by Владимир Рындин on 11/25/18.
+//  Copyright © 2018 com.ryndinvi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
